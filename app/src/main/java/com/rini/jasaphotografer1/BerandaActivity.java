78a8b2bf66d61f638afe721ea7428188a5d1cd2c
Rini <riniman796@gmail.com>
2022-06-24 23:12:23 +0800

    baraction

--- app/src/main/java/com/rini/jasaphotografer1/BerandaActivity.java
+++ app/src/main/java/com/rini/jasaphotografer1/BerandaActivity.java
@@ -2,7 +2,11 @@ package com.rini.jasaphotografer1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class BerandaActivity extends AppCompatActivity {
 
@@ -11,4 +15,25 @@ public class BerandaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beranda);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.optionmenu, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.history){
+            startActivity(new Intent(this, HistoryActivity.class));
+        } else if (item.getItemId() == R.id.Logout) {
+            startActivity(new Intent(this, LogoutActivity.class));
+        }
+
+        return true;
+    }
 }
+
---
+++ app/src/main/java/com/rini/jasaphotografer1/HistoryActivity.java
@@ -0,0 +1,16 @@
+package com.rini.jasaphotografer1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+    }
+}
